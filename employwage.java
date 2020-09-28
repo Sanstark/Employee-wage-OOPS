80cd4495e590a33b1a82066bb209aff9b4cc7186
Sanstark <47468504+Sanstark@users.noreply.github.com>
2020-09-28 20:08:50 +0530

    Add files via upload

---
+++ employwage.java
@@ -0,0 +1,14 @@
+package empwage.wage;
+
+/**
+ * Hello world!
+ *
+ */
+public class employwage
+
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee Wage Computation" );
+    }
+}
